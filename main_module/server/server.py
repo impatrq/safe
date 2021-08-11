5acc8fff5a11d24a969e73a17f2241d571579820
Mendo <mendoza.julian.lionel@gmail.com>
2021-08-11 15:58:24 -0300

    refactor: delete unnecessary css assets and modify code

--- main_module/server/server.py
+++ main_module/server/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return render_template('index.html')
+    return render_template('default.html')
 
 if __name__ ==  '__main__':
     app.config['TESTING'] = True
