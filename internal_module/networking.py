b6cc5bec59bc6e8e3be1f295ef6c5849c9e407cc
lautipaletta <lautaropaletta1@gmail.com>
2021-08-11 14:47:40 -0300

    feat: add networking python file with wifi connection function

--- internal_module/networking.py
+++ internal_module/networking.py
@@ -1,15 +1,16 @@
 import network
 
+WIFI_SSID = 'Paletta_WiFi'
+WIFI_PASSWORD = 'tele-2979543'
+
 def connect_to_wifi():
     sta_if = network.WLAN(network.STA_IF)
     
     if not sta_if.isconnected():
-        print('Connecting to network...')
+        print('Connecting to network ' + WIFI_SSID + ' ...')
         sta_if.active(True)
-        sta_if.connect('Paletta_WiFi', 'tele-2979543')
+        sta_if.connect(WIFI_SSID, WIFI_PASSWORD)
         while not sta_if.isconnected():
             pass
-        
-    print('Network config:', sta_if.ifconfig())
-    
-do_connect()
+        print('Connected!')
+
