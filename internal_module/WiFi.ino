79c32420f654bf9fa94ab649f0132dcc85fd1903
Ronda <agusleonel.r@gmail.com>
2021-09-18 23:54:55 -0500

    feat: add function to make post request

--- internal_module/WiFi.ino
+++ internal_module/WiFi.ino
@@ -1,4 +1,11 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+// Informacion para el post
+const char* SECRET_KEY = "SECRET_KEY";
+const char* ENV_UPDATE_URL = "http://...:8000/api/modules/env_update/";
+const char* DOOR_MAC = "DOOR_MAC";
 
 // Datos de red
 const char* ssid = "SSID";
@@ -21,4 +28,40 @@ void conect_to_wifi() {
         delay(500);
     }
     Serial.prtint("b");                                            // Para avisar que se termino de conectar al wifi
+}
+
+String env_update(float co_level, float co2_level, float metano_level, float lpg_level) {
+  if (WiFi.status() == WL_CONNECTED) {                               // Si esta conectado al WiFi
+
+    WiFiClient client;  
+    HTTPClient http;
+
+    http.begin(client, ENV_UPDATE_URL);                         // Creamos la conexion http
+    
+    DynamicJsonDocument doc(2048);                              // Creamos un documento dinamico de Json e indicamos su espacio
+
+    // Asignamos los valores con la determinada clave valor 
+    doc["SECRET_KEY"] = SECRET_KEY;                           
+    doc["door_mac"] = DOOR_MAC;
+    doc["co_level"] = co_level;
+    doc["co2_level"] = co2_level;
+    doc["metano_level"] = metano_level;
+    doc["lpg_level"] = lpg_level;
+
+    String json;                                                // Creamos una variable json de tipo String
+    
+    serializeJson(doc, json);                                   // Serealizamos lo escrito en el documento creado previamente y lo guardamos en la variable json
+            
+    // Enviamos el post
+    http.addHeader("Content-Type", "application/json");
+    int httpResponseCode = http.POST(json);
+
+    if (httpResponseCode == 200) {       
+      String response = http.getString();                       
+      return response;
+    }
+    else {
+      return "Post Failed";
+    }        
+  }
 }
