97457c9394c46ee3e5c7d7620f5206e530f924ee
Julián Mendoza <mendoza.julian.lionel@gmail.com>
2021-09-28 23:26:55 -0300

    feat.: create new templeate

--- main_module/server/server.py
+++ main_module/server/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return render_template('default.html')
+    return render_template('loading.html')
 
 if __name__ ==  '__main__':
     app.config['TESTING'] = True
